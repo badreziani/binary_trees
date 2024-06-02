3c73b7aa0dbd4b49742f2aac1c6422a61d38eeb7
Badre Ziani <badreziani@gmail.com>
2024-06-02 21:44:04 +0100

    Create task 0

--- binary_trees.h
+++ binary_trees.h
@@ -1,5 +1,6 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
+#include <stdlib.h>
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -16,4 +17,8 @@ struct binary_tree_s
 	struct binary_tree_s *right;
 };
 typedef struct binary_tree_s binary_tree_t;
+/** Task functions **/
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+void binary_tree_print(const binary_tree_t *);
 #endif
