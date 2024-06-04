c02713a067245fd60e9c185c0f5054a7da7525df
7amzawey <mohamedhamza123467890@gmail.com>
2024-06-04 22:53:30 +0200

    fine

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
- * binary_tree_leaves - counts the leaves in a binary tree.
+ * binary_tree_leaves - counts the leaves in the binary tree.
  * @tree: is a pointer to the root node of the tree.
  * Return: 0 if tree == NULL or the Number of leaves
  */
