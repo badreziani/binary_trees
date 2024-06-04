ab8e15056277eb51302bb200031b8741c51926ee
7amzawey <mohamedhamza123467890@gmail.com>
2024-06-04 15:39:19 +0200

    are you the root git add .

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_root - checks if a given node is a root
+ * @node: the node to check
+ * Return: 1 if node 0 if not and if NULL
+ */
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+
+	if (!node->parent)
+		return (1);
+	else
+		return (0);
+}
