90dc53f9856918d76dca709d0f0aea0d2ecff192
Badre Ziani <badreziani@gmail.com>
2024-06-04 22:26:19 +0100

    Update

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -8,7 +8,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-	if (!tree->right && !tree->left && tree->parent)
+	if (!tree->right && !tree->left)
 		return (1);
 	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
