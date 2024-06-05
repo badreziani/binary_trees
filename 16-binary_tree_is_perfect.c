908cd97757d89e90f9cc493212e70d0c60e4e0d5
7amzawey <mohamedhamza123467890@gmail.com>
2024-06-05 15:52:25 +0200

    some updates

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -11,6 +11,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (!tree->left && !tree->right)
 		return (1);
 	if (tree->left && tree->right)
-		return (binary_tree_is_perfect(tree->left) && binary_tree_is_full(tree->right));
+		return (binary_tree_is_perfect(
+					tree->left) && binary_tree_is_perfect(
+						tree->right));
 	return (0);
 }
