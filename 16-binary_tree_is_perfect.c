0423339116646e5c34c78fc4ca5afde40c75344f
Badre Ziani <badreziani@gmail.com>
2024-06-05 14:01:00 +0100

    Create task 17

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_perfect - checks if a binary tree is perfect
+ * @tree: the tree
+ * Return: 0 if tree is NULL or 1
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	if (!tree->left && !tree->right)
+		return (1);
+	if (tree->left && tree->right)
+		return (binary_tree_is_perfect(tree->left) && binary_tree_is_full(tree->right));
+	return (0);
+}
